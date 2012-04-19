92118403cc4e1b5ae051ce12b32bebe8f94c50e5
Cory G Watson <gphat@onemogin.com>
2012-04-18 21:17:06 -0500

    Increase public timeline bits from 10 to 25 items.

--- src/main/scala/practice/daily/tyler/Public.scala
+++ src/main/scala/practice/daily/tyler/Public.scala
@@ -10,7 +10,7 @@ class Public() {
   private val log = Logger.get(getClass)
   implicit val formats = DefaultFormats // Brings in default date formats etc.
 
-  def getUserTimeline(userId : Int, page : Int = 1, count : Int = 10) : List[Map[String,Any]] = {
+  def getUserTimeline(userId : Int, page : Int = 1, count : Int = 25) : List[Map[String,Any]] = {
 
     val es = new ElasticSearch("tdp-actions")
     es.getPublicTimeline(Some(userId), page, count)
@@ -19,7 +19,7 @@ class Public() {
   /**
   * Get the public timeline
   */
-  def getTimeline(page : Int = 1, count : Int = 10) : Map[String,Any] = {
+  def getTimeline(page : Int = 1, count : Int = 25) : Map[String,Any] = {
 
     val es = new ElasticSearch("tdp-actions")
     val tl = es.getPublicTimeline(None, page, count)
